135a04ea1c98ceedb1d09287b311236cefec38aa
unknown <kevers429@gmail.com>
2020-02-08 00:25:26 -0700

    server side of setup-dht

--- p1/client.py
+++ p1/client.py
@@ -3,7 +3,10 @@ import pickle
 import socket
 import sys
 
+from collections import namedtuple
 from types import SimpleNamespace as sn
+
+User = namedtuple('User', 'user_name ipv4 port')
 
 def main():
     display_help()
@@ -20,11 +23,13 @@ def interpret_command(command):
         command_split = command.split(' ')
         if command_split[0] == 'register':
             register(*command_split[1:])
+        elif command_split[0] == 'setup-dht':
+            setup_dht(*command_split[1:])
         else:
             print("Command not understood, try again.")
 
 def display_help():
-    print('Available commands:')
+    print('\nAvailable commands:')
     print('help')
     print('register <user-name> <IPv4-address> <port>')
     print('setup-dht <n> <user-name>')
@@ -38,6 +43,15 @@ def register(user_name, ipv4, port):
     response = sock.recv(1024)
     data = pickle.loads(response)
     print(data.status)
+
+def setup_dht(n, user_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    request = sn(command='setup-dht', args=sn(n=int(n), user_name=user_name))
+    sock.sendto(pickle.dumps(request), (args.host, args.host_port))
+    response = sock.recv(1024)
+    data = pickle.loads(response)
+    print(data.status)
+    print(data.body)
 
 
 parser = argparse.ArgumentParser(description='Client process that tracks teh state of clients')
--- p1/server.py
+++ p1/server.py
@@ -1,19 +1,23 @@
 import argparse
 import pickle
+import random
 import socket
 
 from collections import namedtuple
 from types import SimpleNamespace as sn
 
+User = namedtuple('User', 'user_name ipv4 port')
+
 class Server:
     def __init__(self):
-        self.registered_users = []
-        self.User = namedtuple('User', 'user_name ipv4 port')
+        self.registered_users = {}
+        self.user_state = {}
+        self.num_DHTs = 0
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((socket.gethostname(), args.port))
         while True:
             self.bytes, self.addr = self.sock.recvfrom(1024)
-            print('data from', self.addr)
+            print('Receiving data from', self.addr)
             self.data = pickle.loads(self.bytes)
             self.handle_datagram()
 
@@ -30,14 +34,27 @@ class Server:
         if self.data.command == 'register':
             if self.data.args.ipv4 != self.addr[0] or self.data.args.port > 65535 or len(self.data.args.user_name) > 15:
                 return self.failure()
-            for user, ipv4, port in self.registered_users:
-                if self.data.args.user_name == user or self.data.args.port == port:
+            for user in self.registered_users:
+                if user == self.data.args.user_name or self.registered_users[user].port == self.data.args.port:
                     return self.failure()
-
-            self.registered_users.append(self.User(**self.data.args.__dict__))
+            # User is unique and valid, add to registered users
+            self.registered_users[self.data.args.user_name] = User(**self.data.args.__dict__)
+            self.user_state[self.data.args.user_name] = 'Free'
+            print(f'Successfully registered user: {User(**self.data.args.__dict__)}')
             self.success()
-            print('registered users:', self.registered_users)
-
+        elif self.data.command == 'setup-dht':
+            if (self.registered_users.get(self.data.args.user_name) is None or self.data.args.n < 2
+                or len(self.registered_users) < self.data.args.n or self.num_DHTs > 0):
+                return self.failure()
+            # Begin setup of DHT
+            self.user_state[self.data.args.user_name] = 'Leader'
+            dht_users = [self.data.args.user_name]
+            for _ in range(1, self.data.args.n):
+                random_free_user = random.choice([user for user in self.user_state if self.user_state[user] == 'Free'])
+                dht_users.append(random_free_user)
+                self.user_state[random_free_user] = 'InDHT'
+            self.num_DHTs += 1
+            self.success(body=[self.registered_users[user] for user in dht_users])
 
 
 parser = argparse.ArgumentParser(description='Server process that tracks teh state of clients')
